bd06691c3e8cf92023ec8c2b24b2a39c87090911
shandilya1998 <shandilya.shreyas@gmail.com>
2024-11-19 22:11:09 +0530

    MORB_SLAM integration
    Problem
    =======
    1. node for MORB_SLAM to needs to be implemented
    
    Solution
    ========
    1. created a common monocular node for slam, with each slam type
       integrated through the same class
    2. the choice for slam is set in each of the slam executable in
       CMakeLists.txt through compile time definitions
    3. moved MonocularSlamNode from src/orbslam3/monocular.* to
       src/monocular.* to enforce code reusability
    4. created src/morbslam/monocular.* with all the required APIs for call
       within MonocularSlamNode
    
    Note
    ====
    Not tested on data. Next step is testing on video data

--- include/slam/morbslam/monocular.hpp
+++ include/slam/morbslam/monocular.hpp
@@ -63,33 +63,4 @@ class MonoMORBSLAM : public Slam{
 		};
 };
 typedef MonoMORBSLAM MonoMORBSLAM;
-
-class MonocularSlamNode : public SlamNode
-{
-	public:
-		MonocularSlamNode();
-		~MonocularSlamNode();
-	private:
-		// Node Configurations
-		std::string mpCameraTopicName;
-		std::string mpSlamSettingsFilePath;
-		std::string mpVocabFilePath;
-		std::unique_ptr<Slam> mpSlam = nullptr;
-		// ORB_SLAM3 related attributes
-		ORB_SLAM3::Tracking::eTrackingState mpState;
-		// Image pointer for receiving and passing images to SLAM
-		cv_bridge::CvImagePtr m_cvImPtr;
-		// Publishers, Subscribers, Services and Actions
-		rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr mpFrameSubscriber;
-		rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr mpAnnotatedFramePublisher;
-		rclcpp::Service<custom_interfaces::srv::StartupSlam>::SharedPtr mpSlamStartupService;
-		rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr mpSlamShutdownService;
-		// Callbacks and Methods
-		void Update();
-		void InitialiseSlamNode(
-				std::shared_ptr<custom_interfaces::srv::StartupSlam::Request> request,
-				std::shared_ptr<custom_interfaces::srv::StartupSlam::Response> response);
-		void GrabImage(const sensor_msgs::msg::Image::SharedPtr msg);
-		void PublishFrame();
-};
 #endif
